db98ea99e040cd577215c5363610902875576599
sumit <pksumitkumar98@gmail.com>
2021-08-27 14:49:12 +0530

    FVs

--- backend/userroute.js
+++ backend/userroute.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // mongodb+srv://capgemini:capgemini@cluster0.wxqs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-mongoose.connect('mongodb://localhost/task', { useFindAndModify: false }).then(e => {
+mongoose.connect('mongodb+srv://capgemini:capgemini@cluster0.wxqs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useFindAndModify: false }).then(e => {
     console.log('config connected')
 }).catch(err => {
     console.log(err.message)
